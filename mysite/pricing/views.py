5d01688742e783d8b6e0a931d64bd8d38a88416c
satyammaheshwari <satyam.maheshwari@3pillarglobal.com>
2013-09-10 12:14:37 +0530

    committing pricing project

---
+++ mysite/pricing/views.py
@@ -0,0 +1,86 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from lxml import html
+import re
+import urllib2
+
+
+
+def index(request):
+#     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = RequestContext(request, {
+#         'latest_poll_list': latest_poll_list,
+#     })
+#     return HttpResponse(template.render(context))
+
+#    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'results': None}
+
+    return render_to_response('pricing/index.html', context)
+
+
+def detail(request):
+#     try:
+#         poll = Poll.objects.get(pk=poll_id)
+#     except Poll.DoesNotExist:
+#         raise Http404
+#     return render(request, 'polls/detail.html', {'poll': poll})
+#    poll = get_object_or_404(pricing, pk=poll_id)
+    url1 = request.GET['1']
+    url2 = request.GET['2']
+    url3 = request.GET['3']
+    url4 = request.GET['4']
+    url5 = request.GET['5']
+    
+    prices = []
+    for u in [url1, url2, url3, url4, url5]:
+        try:
+            op = urllib2.urlopen(u)
+            src = op.read()
+            if 'mydistance-learning' in u:
+                parsed_src = html.fromstring(src)
+                price = parsed_src.xpath("//span[@class='price']/text()")
+#                price = re.findall('<span id="sp_price" class="total-value">(.*?)</', src)
+                if price:
+                    prices.append(price[0])
+                else:
+                    prices.append(u)
+            elif 'distance-learning-centre.' in u:
+                parsed_src = html.fromstring(src)
+                price = parsed_src.xpath("//span[@class='price']/text()")
+#                price = re.findall('<span class="price">(.*?)</', src)
+                if price:
+                    prices.append(price[0])
+                else:
+                    prices.append(u)
+            elif 'openstudycollege' in u:
+                parsed_src = html.fromstring(src)
+                price = parsed_src.xpath("//span[@id='fullpaymentprice']/text()")
+#                price = re.findall('<span id="fullpaymentprice">(.*?)</', src)
+                if price:
+                    prices.append(price[0])
+                else:
+                    prices.append(u)
+            elif 'ukopencollege' in u:
+                parsed_src = html.fromstring(src)
+                price = parsed_src.xpath("//option[contains(text(),'Pay in Full')]/text()")
+#                price = re.findall('Pay in Full (.*?)</', src)
+                if price:
+                    prices.append(price[0].rsplit(' ',1)[-1])
+                else:
+                    prices.append(u)
+            elif 'edistancelearning' in u:
+                parsed_src = html.fromstring(src)
+                price = parsed_src.xpath("//td[contains(text(),'Enrolment Fee')]/following-sibling::td[1]/text()")
+#                price = re.findall('<td class="bodytext">(.*?)</', src)
+                if price:
+                    prices.append(price[0])
+                else:
+                    prices.append(u)
+        except ValueError, e:
+            prices.append("Not a valid URL")
+    context = {'results': prices}
+    return render_to_response('pricing/index.html', context,
+                                                    context_instance=RequestContext(request))# Create your views here.
