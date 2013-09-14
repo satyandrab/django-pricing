4dd3733e4a10846a236ccf0066a74a03e63d07dd
satyandrab <root@vps33549.ovh.net>
2013-09-14 18:15:13 +0200

    auto dev server commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'D:/django_workspace/mysiteproj/src/sqlite3.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/django_pricing/mysite/data.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -100,10 +100,10 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
-                 'D:/django_workspace/django-pricing/mysiteproj/src/mysite/templates'
+                 '/home/django_pricing/mysite/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
